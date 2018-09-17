34afc5c0cf627d980f7b06322a03c4ace076deb5
Alex M <alexgotfrag@gmail.com>
2018-09-17 12:06:17 +0300

    Renamed fields

--- Assets/MarchingCubesGPU/Scripts/MarchingCubesTables.cs
+++ Assets/MarchingCubesGPU/Scripts/MarchingCubesTables.cs
@@ -13,7 +13,7 @@ namespace MarchingCubesGPUProject
         // This table lists the edges intersected by the surface for all 256 possible vertex states
         // There are 12 edges.  For each entry in the table, if edge #n is intersected, then bit #n is set to 1
         // cubeEdgeFlags[256]
-        public readonly static int[] CubeEdgeFlags = new int[]
+        public static readonly int[] CubeEdgeFlags = new int[]
         {
         0x000, 0x109, 0x203, 0x30a, 0x406, 0x50f, 0x605, 0x70c, 0x80c, 0x905, 0xa0f, 0xb06, 0xc0a, 0xd03, 0xe09, 0xf00,
         0x190, 0x099, 0x393, 0x29a, 0x596, 0x49f, 0x795, 0x69c, 0x99c, 0x895, 0xb9f, 0xa96, 0xd9a, 0xc93, 0xf99, 0xe90,
@@ -39,7 +39,7 @@ namespace MarchingCubesGPUProject
         //  For example: triangleConnectionTable[3] list the 2 triangles formed when corner[0] 
         //  and corner[1] are inside of the surface, but the rest of the cube is not.
         //  triangleConnectionTable[256][16]
-        public readonly static int[,] TriangleConnectionTable = new int[,]
+        public static readonly int[,] TriangleConnectionTable = new int[,]
         {
         {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1},
         {0, 8, 3, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1},
